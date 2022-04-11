9d11badffe7dc343aed88cdd58e4eb0f777a2387
Mlink <lmo9903@gmail.com>
2022-04-11 22:39:42 +0900

    class 함수 추가 정의

--- findRoot.py
+++ findRoot.py
@@ -13,13 +13,14 @@
     출력
     첫째 줄에 프로그램이 구한 경로의 점수를 출력한다. 둘째 줄에는 이동 경로를 나타내는 문자들을 빈 칸을 사이에 두고 출력한다. 왼쪽은 L, 오른쪽은 R, 위쪽은 U, 아래쪽은 D로 나타내며, A[1][1]에서부터의 이동을 순서대로 출력하면 된다.
 """
+# 위왼쪽 -1 아래오른쪽 1
 U_L = -1
 D_R = 1
-START = "s"
-UP = "u"
-DOWN = "d"
-LEFT = "l"
-RIGHT = "r"
+# 방향의 인덱스 순서
+RIGHT = 0
+DOWN = 1
+LEFT = 2
+UP = 3
 
 typed = int(input())
 
@@ -38,6 +39,83 @@ class Route():
     def pathInsert(self, road):
         self.path.append(road)
 
+    def expensiveOne(self):
+        is_walk_ok = self.searchOfPossiblePath()
+        checked_value = []
+
+        for on, ok in enumerate(is_walk_ok):
+            if not ok:
+                checked_value.append(-200)
+
+            else:
+                checked_value.append(self.callValue(on))
+
+        value = max(checked_value)
+
+        return checked_value.index(value), max(checked_value)
+
+    def callValue(self, direction):
+        now_x, now_y = self.path_now_x, self.path_now_y
+        to_check_x, to_check_y = self.nextPos(now_x, now_y)
+
+        if direction == RIGHT:
+            return self.path[to_check_x[RIGHT]][to_check_y[RIGHT]]
+
+        elif direction == DOWN:
+            return self.path[to_check_x[DOWN]][to_check_y[DOWN]]
+        
+        elif direction == LEFT:
+            return self.path[to_check_x[LEFT]][to_check_y[LEFT]]
+
+        else:
+            return self.path[to_check_x[UP]][to_check_y[UP]]
+
+    def searchOfPossiblePath(self):
+        now_x, now_y = self.path_now_x, self.path_now_y
+        to_check_x, to_check_y = self.nextPos(now_x, now_y)
+        can_go = []
+
+        for pos_x, pos_y in zip(to_check_x, to_check_y):
+            can_go.append(self.checkNextPath(pos_x, pos_y))
+
+        return can_go
+
+    def nextPos(self, now_x, now_y):
+        return [now_x, now_x + D_R, now_x, now_x + U_L], [now_y + D_R, now_y, now_y + U_L, now_y]
+
+    def checkNextPath(self, pos_x, pos_y):
+        next_path = str(f"{pos_x},{pos_y}")
+
+        if self.isPassOk(pos_x, pos_y):
+            result = True
+
+        else:
+            result = False
+
+        if result == True:
+            if not self.isPassBefore(next_path):
+                result = True
+
+            else:
+                result = False
+        
+        return result
+
+    def isPassOk(self, pos_x, pos_y):
+        if pos_x < 0 or pos_x >= self.end_point or pos_y < 0 or pos_y >= self.end_point:
+            return False
+        
+        else:
+            return True
+
+    def isPassBefore(self, path_index):
+        try:
+            if self.path_before.index(path_index) >= 0:
+                return True
+        
+        except:
+            return False
+
 
 def typingPath(path_count):
     route = Route(path_count)
@@ -46,6 +124,7 @@ def typingPath(path_count):
         route.pathInsert(splitAndInt(input()))
 
     print(route.total_score)
+    print(route.expensivePath())
 
 def splitAndInt(list):
     return [int(a) for a in list.split()]
