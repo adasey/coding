0e0b800af3ba50cc560ed3cbdc43f097895fe0e0
Mlink <lmo9903@gmail.com>
2022-04-22 23:55:20 +0900

    ??! 백준 문제 풀이

---
+++ BaekjoonQuiz/StepPrint/Question.java
@@ -0,0 +1,15 @@
+package BaekjoonQuiz.StepPrint;
+
+import java.util.Scanner;
+
+public class Question {
+    static public void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String typed;
+        typed = sc.nextLine();
+
+        System.out.println(typed + "??!");
+
+        sc.close();
+    }
+}
